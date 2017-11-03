1a2e2f4bb4fcdb4b197adadce94d575528b5e2eb
jaisharma123 <j11798s@gmail.com>
2017-11-03 21:41:02 +0530

    modified log in parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,37 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
 
-def full_name
+  def full_name
     "#{self.first_name} #{self.last_name}"
   end
 
-def admin?
+  def admin?
     self.role == "admin"
   end
 
-def user?
+  def user?
     self.role == "user"
   end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.mobile_no || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(mobile_no) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:mobile_no) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+
 end
